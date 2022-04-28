6a3872a59e755060895ec8f617e0fafdf85ef5fe
Daniel DAbramo <daniel.dabramo@lovetoknow.com>
2022-04-28 12:22:03 +0200

    feat(eslint-config): add all eslint-plugin-vuejs-accessibility rules as warning
    BREAKING CHANGE: apps/modules that consume this package will have to install eslint-plugin-vuejs-accessibility

--- packages/eslint-config/vue.js
+++ packages/eslint-config/vue.js
@@ -1,5 +1,19 @@
+const vueA11y = require('eslint-plugin-vuejs-accessibility')
+
+// Take all accessibility rules, but set them as warning only!
+const a11yAsWarning = Object.keys(vueA11y.rules).reduce(
+  (rules, ruleName) =>
+    Object.assign({}, rules, { [`vuejs-accessibility/${ruleName}`]: 1 }),
+  {}
+)
+
 module.exports = {
-  extends: ['plugin:vue/essential', './index.js'],
+  extends: [
+    'plugin:vue/essential',
+    './index.js',
+    'plugin:vuejs-accessibility/recommended',
+  ],
+  plugins: ['vuejs-accessibility'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     sourceType: 'module',
@@ -10,44 +24,48 @@ module.exports = {
     node: true,
     es6: true,
   },
-  rules: {
-    'vue/no-use-v-if-with-v-for': [
-      'error',
-      {
-        allowUsingIterationVar: true,
-      },
-    ],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
-    'vue/attributes-order': [
-      'error',
-      {
-        order: [
-          'DEFINITION',
-          'LIST_RENDERING',
-          'UNIQUE',
-          'CONDITIONALS',
-          'RENDER_MODIFIERS',
-          'GLOBAL',
-          'SLOT',
-          'OTHER_DIRECTIVES',
-          'TWO_WAY_BINDING',
-          'CONTENT',
-          'OTHER_ATTR',
-          'EVENTS',
-        ],
-      },
-    ],
-    'vue/no-v-html': 'warn',
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: { normal: 'always', void: 'always' },
-      },
-    ],
-    // Disabling some rules (also will affect Nuxt config)
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/html-indent': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/singleline-html-element-content-newline': 'off', // not very friendly with prettier and the printWidth option
-  },
+  rules: Object.assign(
+    {},
+    {
+      'vue/no-use-v-if-with-v-for': [
+        'error',
+        {
+          allowUsingIterationVar: true,
+        },
+      ],
+      'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'DEFINITION',
+            'LIST_RENDERING',
+            'UNIQUE',
+            'CONDITIONALS',
+            'RENDER_MODIFIERS',
+            'GLOBAL',
+            'SLOT',
+            'OTHER_DIRECTIVES',
+            'TWO_WAY_BINDING',
+            'CONTENT',
+            'OTHER_ATTR',
+            'EVENTS',
+          ],
+        },
+      ],
+      'vue/no-v-html': 'warn',
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: { normal: 'always', void: 'always' },
+        },
+      ],
+      // Disabling some rules (also will affect Nuxt config)
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/html-indent': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off', // not very friendly with prettier and the printWidth option
+    },
+    a11yAsWarning
+  ),
 }
