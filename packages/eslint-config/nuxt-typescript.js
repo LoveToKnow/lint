bfab5d1f0b2ae1177d23e64ca8ba7226708ab714
Lluc Pages <lluc.pages@lovetoknow.com>
2023-11-21 12:03:13 +0100

    fix: nuxt typescript unused vars

--- packages/eslint-config/nuxt-typescript.js
+++ packages/eslint-config/nuxt-typescript.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript', require.resolve('./nuxt.js')],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
 }
